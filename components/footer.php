4f68598bd5f7410a8d8a116679be477f4cd2da3a
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-16 14:48:57 +0530

    Updated

--- components/footer.php
+++ components/footer.php
@@ -2,12 +2,6 @@
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
-<!-- JS -->
-<!-- <script src="assets/js/loader.js"></script> -->
-
-<!-- JQUERY -->
-<!-- <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script> -->
-
 <!-- Lottie -->
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 
