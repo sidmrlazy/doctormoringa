7b0fa63908e459be44c228d369fdfbd86ded8b1a
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-05-11 14:11:00 +0530

    Updated Checkout Screen and Cart Screen

--- components/cart/cart-screen.php
+++ components/cart/cart-screen.php
@@ -7,36 +7,82 @@
         </button>
     </div>
 
-    <form method="POST" class="main-cart">
-        <?php
-        include('admin/includes/server/config.php');
-
-        $get_products_query = "SELECT * FROM items";
-
-        ?>
-        <div class="cart-product-img">
-            <img src="assets/images/background/moringa-leaves.jpg" alt="">
-        </div>
-        <div class="cart-product-details">
-            <h3>Product Category</h3>
-            <h1>Product Name</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula ut diam tincidunt
-                ullamcorper
-                ut at magna. Sed fermentum in est in elementum. </p>
-        </div>
-
-        <div class="cart-calculator">
-            <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value">
-                <ion-icon name="remove-circle-outline" class="chevron-up-outline"></ion-icon>
+    <form class="w-100" action="" method="POST">
+        <div class="main-cart">
+            <div class="cart-product-img">
+                <img src="assets/images/background/moringa-leaves.jpg" alt="">
             </div>
-            <input type="number" id="number" value="0" />
-            <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value">
-                <ion-icon name="add-circle-outline" class="chevron-down-outline"></ion-icon>
+            <div class="cart-product-details">
+                <h3>Product Category</h3>
+                <h1>Product Name</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula ut diam tincidunt
+                    ullamcorper
+                    ut at magna. Sed fermentum in est in elementum. </p>
+            </div>
+
+            <div class="cart-calculator">
+                <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value">
+                    <ion-icon name="remove-circle-outline" class="chevron-up-outline"></ion-icon>
+                </div>
+                <input type="number" id="number" value="0" />
+                <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value">
+                    <ion-icon name="add-circle-outline" class="chevron-down-outline"></ion-icon>
+                </div>
+            </div>
+
+            <div class="cart-product-price">
+                <p>₹200</p>
             </div>
         </div>
 
-        <div class="cart-product-price">
-            <p>₹200</p>
+        <div class="main-cart">
+            <div class="cart-product-img">
+                <img src="assets/images/background/moringa-leaves.jpg" alt="">
+            </div>
+            <div class="cart-product-details">
+                <h3>Product Category</h3>
+                <h1>Product Name</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed ligula ut diam tincidunt
+                    ullamcorper
+                    ut at magna. Sed fermentum in est in elementum. </p>
+            </div>
+
+            <div class="cart-calculator">
+                <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value">
+                    <ion-icon name="remove-circle-outline" class="chevron-up-outline"></ion-icon>
+                </div>
+                <input type="number" id="number" value="0" />
+                <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value">
+                    <ion-icon name="add-circle-outline" class="chevron-down-outline"></ion-icon>
+                </div>
+            </div>
+
+            <div class="cart-product-price">
+                <p>₹200</p>
+            </div>
+        </div>
+
+        <div class="pricing-tab">
+
+            <div class="inner-headings">
+                <p id="heading">Subtotal</p>
+                <p>Item Price</p>
+            </div>
+
+            <div class="inner-headings">
+                <p id="heading">Shipping</p>
+                <p>Delivery Fee</p>
+            </div>
+
+            <div class="inner-headings">
+                <p id="heading">Grand Total</p>
+                <p>Delivey Fee + Item Price</p>
+            </div>
+
+
+            <!-- Use this button when developing the functionality -->
+            <!-- <button type="submit" name="" class="checkout-btn">Proceed to Checkout</button> -->
+            <a href="checkout" type="submit" name="" class="checkout-btn">Proceed to Checkout</a>
         </div>
     </form>
 
---
+++ components/cart/checkout-form.php
@@ -0,0 +1,48 @@
+<div class="container mt-5 mb-5">
+
+
+    <form method="POST" class="checkout-form">
+
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Full Name">
+            <label for="floatingInput">Full Name</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="floatingInput" placeholder="Contact Number">
+            <label for="floatingInput">Contact Number</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="Email">
+            <label for="floatingInput">Email</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <textarea class="form-control" placeholder="Full Address" id="floatingTextarea2"
+                style="height: 100px"></textarea>
+            <label for="floatingTextarea2">Full Address</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" placeholder="State">
+            <label for="floatingInput">State</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" placeholder="City">
+            <label for="floatingInput">City</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Pincode">
+            <label for="floatingInput">Pincode</label>
+        </div>
+
+        <button type="submit" name="submit" class="payment-btn">
+            Make Payment
+        </button>
+
+
+    </form>
+</div>
