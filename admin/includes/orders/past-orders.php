1aa96d1440d5713934a525fc6664d74b8b88f572
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-06-11 16:29:40 +0530

    Updated

--- admin/includes/orders/past-orders.php
+++ admin/includes/orders/past-orders.php
@@ -3,10 +3,20 @@
 
     <?php
     include 'includes/server/config.php';
-
-    $get_orders_query = "SELECT * FROM uder_order ORDER BY `order_id` DESC";
+    // Pagination
+    $limit = 10;
+    $get_orders_query = "SELECT * FROM uder_order";
     $result = mysqli_query($connection, $get_orders_query);
-
+    $total_rows = mysqli_num_rows($result);
+    $total_pages = ceil($total_rows / $limit);
+    if (!isset($_GET['page'])) {
+        $page_number = 1;
+    } else {
+        $page_number = $_GET['page'];
+    }
+    $initial_page = ($page_number - 1) * $limit;
+    $getQuery = "SELECT * FROM uder_order ORDER BY `order_id` DESC LIMIT " . $initial_page . ',' . $limit;
+    $result = mysqli_query($connection, $getQuery);
     while ($row = mysqli_fetch_assoc($result)) {
         $order_id = $row['order_id'];
         $order_user_name = $row['order_user_name'];
@@ -17,8 +27,8 @@
             $new_order_time =  gmdate("d-m-Y", $order_time_stamp);
         }
         $order_total_amount = $row['order_total_amount'];
-        $order_status = $row['order_status']; ?>
-
+        $order_status = $row['order_status'];
+    ?>
     <div class="past-order-card">
         <div class="col-md-4">
             <p class="customer-name"><?php echo $order_user_name; ?></p>
@@ -54,29 +64,24 @@
                         echo "<p class='status-fail'>Failed</p>";
                     }
                     ?>
-
-                <!-- <p class="status-fail">Failed</p> -->
             </div>
         </div>
     </div>
-    <!-- <nav aria-label="Page navigation example">
-        <ul class="pagination justify-content-center">
-
-            <li class="page-item disabled">
-                <a class="page-link">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
-    </nav> -->
 
     <?php
     }
     ?>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
 
+            <?php
+            for ($page_number = 1; $page_number <= $total_pages; $page_number++) {
 
+                echo '<li class="page-item"><a class="page-link" href="past-orders.php?page=' . $page_number . '">' . $page_number . ' </a></li>';
+            }
+
+            ?>
+
+        </ul>
+    </nav>
 </div>
