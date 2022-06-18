b7e518d182991a27633e168086169ab76b36aed8
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-06-18 10:37:48 +0530

    Updat3ed

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php include('components/header.php') ?>
 <?php //include('components/elements/conditional-login-modal.php')
 ?>
-<?php include('components/maintenance.php')
+<?php //include('components/maintenance.php')
 ?>
-<?php //include('components/navbar.php')
+<?php include('components/navbar.php')
 ?>
-<?php //include('components/elements/banner.php')
+<?php include('components/elements/banner.php')
 ?>
-<?php //include('components/elements/home-page-category-slider.php')
+<?php include('components/elements/home-page-category-slider.php')
 ?>
-<?php //include('components/elements/best-sellers.php')
+<?php include('components/elements/best-sellers.php')
 ?>
-<?php //include('components/elements/blogs-carousel.php')
+<?php include('components/elements/blogs-carousel.php')
 ?>
 <?php //include('components/elements/newsletter.php')
 ?>
-<?php //include('components/elements/customer-feedback.php')
+<?php include('components/elements/customer-feedback.php')
 ?>
-<?php //include('components/footer.php')
+<?php include('components/footer.php')
 ?>
 
 
