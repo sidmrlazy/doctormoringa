92b1792532d476bae4c45261d1944a5a0f236d86
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-06-30 17:03:03 +0530

    OK

--- components/elements/creatives.php
+++ components/elements/creatives.php
@@ -1,18 +1,12 @@
-<div class="container-fluid section-wrapper section-background mt-5 mb-5 w-100">
-
-
+<div class="container-fluid section-background section-wrapper mt-5 mb-5 w-100">
     <div class="custom-image-wrapper">
         <?php
         $query = "SELECT * FROM web_images LIMIT 4";
         $result = mysqli_query($connection, $query);
         while ($row = mysqli_fetch_assoc($result)) {
             $web_img_name = "admin/assets/images/web-imgs/" . $row['web_img_name']; ?>
+
         <img src="<?php echo $web_img_name ?>" class="wrapper-image img-fluid w-100" alt="<?php echo $web_img_name ?>">
-
-        <?php
-        }
-        ?>
+        <?php } ?>
     </div>
-
-
 </div>
--- components/elements/customer-testimonials.php
+++ components/elements/customer-testimonials.php
@@ -1,19 +1,26 @@
 <div class="container mt-5">
-    <h1 class="section-heading">Testimonials</h1>
     <?php
     include('admin/includes/server/config.php');
     $query = "SELECT * FROM `feedback` WHERE feedback_status = 'Live'";
     $result = mysqli_query($connection, $query);
-    while ($row = mysqli_fetch_assoc($result)) {
-        $feedback_item_id = $row['feedback_item_id'];
-        $feedback_user_name = $row['feedback_user_name'];
-        $feedback_content = $row['feedback_content'];
-        $feedback_date = $row['feedback_date'];
-        $item_query = "SELECT * FROM `items` WHERE item_id = $feedback_item_id";
-        $item_result = mysqli_query($connection, $item_query);
-        while ($row = mysqli_fetch_assoc($item_result)) {
-            $item_name = $row['item_name'];
-            $item_image = "admin/assets/images/products/" . $row['item_image']; ?>
+
+    if ($result == "") {
+    ?>
+    <div class="feedback-section d-none mt-3">
+    </div>
+    <?php
+    } else {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $feedback_item_id = $row['feedback_item_id'];
+            $feedback_user_name = $row['feedback_user_name'];
+            $feedback_content = $row['feedback_content'];
+            $feedback_date = $row['feedback_date'];
+            $item_query = "SELECT * FROM `items` WHERE item_id = $feedback_item_id";
+            $item_result = mysqli_query($connection, $item_query);
+            while ($row = mysqli_fetch_assoc($item_result)) {
+                $item_name = $row['item_name'];
+                $item_image = "admin/assets/images/products/" . $row['item_image']; ?>
+    <h1 class="section-heading">Testimonials</h1>
     <div class="feedback-section mt-3">
         <div class="feedback-product-img">
             <img src="<?php echo $item_image; ?>" alt="<?php echo $item_name; ?>">
@@ -26,8 +33,11 @@
         </div>
     </div>
     <?php
+            }
         }
     }
+
+
     ?>
 
 
