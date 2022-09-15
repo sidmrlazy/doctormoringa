ff9974c83bfbffce4fc1645ccdc3bb0456640173
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-09-15 11:55:02 +0530

    modified:   shop.php

--- shop.php
+++ shop.php
@@ -120,15 +120,36 @@
                         <div class="carousel-item active">
                             <img src="<?php echo $item_image ?>" class="d-block w-100 car-img" alt="...">
                         </div>
+                        <?php if (!file_exists($item_image_2)) { ?>
+                        <div class="d-none carousel-item">
+                            <img src="<?php echo $item_image_2 ?>" class="d-block w-100 car-img" alt="...">
+                        </div>
+                        <?php } else { ?>
                         <div class="carousel-item">
                             <img src="<?php echo $item_image_2 ?>" class="d-block w-100 car-img" alt="...">
                         </div>
+                        <?php } ?>
+
+                        <?php if (!file_exists($item_image_3)) { ?>
+                        <div class="d-none carousel-item">
+                            <img src="<?php echo $item_image_3 ?>" class="d-block w-100 car-img" alt="...">
+                        </div>
+                        <?php } else { ?>
                         <div class="carousel-item">
                             <img src="<?php echo $item_image_3 ?>" class="d-block w-100 car-img" alt="...">
                         </div>
+                        <?php  } ?>
+
+                        <?php if (!file_exists($item_image_4)) { ?>
+                        <div class="d-none carousel-item">
+                            <img src="<?php echo $item_image_4 ?>" class="d-block w-100 car-img" alt="...">
+                        </div>
+                        <?php } else { ?>
                         <div class="carousel-item">
                             <img src="<?php echo $item_image_4 ?>" class="d-block w-100 car-img" alt="...">
                         </div>
+                        <?php } ?>
+
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#<?php echo $new_item_id ?>"
                         data-bs-slide="prev">
