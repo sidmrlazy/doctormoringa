0b52006d9fd68d76eb1349f6ec846179762834ae
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-09-30 13:13:28 +0530

    new file:   assets/images/banners/4.jpeg new file:   assets/images/banners/5.jpeg new file:   assets/images/banners/6.jpeg modified:   components/elements/banner.php

--- components/elements/banner.php
+++ components/elements/banner.php
@@ -10,26 +10,18 @@ console.log(video);
             aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        <!-- <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button> -->
     </div>
     <div class="carousel-inner">
-        <a href="shop.php#2" class="carousel-item centered-carousel active" data-bs-interval="3000">
-            <img src="assets/images/banners/offer.jpg" class="d-block img-fluid" alt="...">
-            <!-- <video class="banner-vid" autoplay controls id="bannerVid">
-                <source src="assets/video/vid-1.mp4" type="video/mp4">
-                <source src="movie.ogg" type="video/ogg">
-            </video> -->
+        <a href="shop.php#Beauty" class="carousel-item centered-carousel active" data-bs-interval="3000">
+            <img src="assets/images/banners/4.jpeg" class="d-block img-fluid" alt="...">
         </a>
-        <div class="carousel-item centered-carousel" data-bs-interval="3000">
-            <img src="assets/images/banners/1.png" class="d-block img-fluid" alt="...">
-        </div>
-        <div class="carousel-item centered-carousel" data-bs-interval="2000">
-            <img src="assets/images/banners/2.png" class="d-block img-fluid" alt="...">
+        <a href="shop.php#Beauty" class="carousel-item centered-carousel" data-bs-interval="3000">
+            <img src="assets/images/banners/5.jpeg" class="d-block img-fluid" alt="...">
+        </a>
+        <a href="shop.php#Beauty" class="carousel-item centered-carousel" data-bs-interval="2000">
+            <img src="assets/images/banners/6.jpeg" class="d-block img-fluid" alt="...">
 
-        </div>
-        <div class="carousel-item centered-carousel">
-            <img src="assets/images/banners/3.png" class="d-block img-fluid" alt="...">
-
-        </div>
+        </a>
     </div>
 </div>
