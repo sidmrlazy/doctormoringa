98561b6e50d1de0df47b2ff5fed6f1d50fdf0844
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-10-13 14:37:09 +0530

    modified:   assets/css/style.css modified:   components/elements/home-page-category-slider.php

--- components/elements/home-page-category-slider.php
+++ components/elements/home-page-category-slider.php
@@ -1,20 +1,20 @@
 <div class="container mt-4 category-slider">
-    <?php
-    include('admin/includes/server/config.php');
-    $query = "SELECT * FROM category";
-    $result = $connection->query($query);
-    while ($row = mysqli_fetch_assoc($result)) {
-        $category_image = $row['category_image'];
-        $category_name =  $row['category_name'];
-        $new_category_name = trim($category_name, " ");
+    <div class="inner-card-grid-mobile">
+        <?php
+        include('admin/includes/server/config.php');
+        $query = "SELECT * FROM category";
+        $result = $connection->query($query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $category_image = $row['category_image'];
+            $category_name =  $row['category_name'];
+            $new_category_name = trim($category_name, " ");
 
-    ?>
-    <a href="shop#<?php echo $category_name  ?>" class="inner-card" id="inner-card-pale">
-        <img src="assets/images/category-icons/<?php echo $category_image ?>" />
-        <p class="text-center"><?php echo $new_category_name; ?></p>
-    </a>
-    <?php
-    }
+        ?>
 
-    ?>
+        <a href="shop#<?php echo $category_name  ?>" class="inner-card inner-card-mobile" id="inner-card-pale">
+            <img src="assets/images/category-icons/<?php echo $category_image ?>" />
+            <p class="text-center"><?php echo $new_category_name; ?></p>
+        </a>
+        <?php } ?>
+    </div>
 </div>
